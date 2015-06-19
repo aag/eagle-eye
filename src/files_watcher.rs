464c3502d0de7d5a1f46e878c95d1221d9f2f303
Adam Goforth <code@adamgoforth.com>
2015-06-19 22:54:41 +0200

    Return file path information with events
    This version clones both the event and the path, so it's probably not as
    efficient as it could be.

--- src/files_watcher.rs
+++ src/files_watcher.rs
@@ -6,32 +6,60 @@ use inotify::wrapper::Event;
 use inotify::wrapper::Watch;
 use inotify::ffi::*;
 
-use std::path::Path;
-
+use std::collections::HashMap;
+use std::path::PathBuf;
 
 pub struct FilesWatcher {
-    inotify: INotify
+    inotify: INotify,
+    watches: HashMap<Watch, PathBuf>
 }
 
 impl FilesWatcher {
     pub fn new() -> FilesWatcher {
         let inotify = INotify::init().unwrap();
 
-        FilesWatcher { inotify: inotify }
+        FilesWatcher { 
+            inotify: inotify,
+            watches: HashMap::new()
+        }
     }
 
-    pub fn add_file(&mut self, path: &Path) -> Watch {
-        self.inotify.add_watch(path, IN_MODIFY | IN_DELETE).unwrap()
+    pub fn add_file(&mut self, path: PathBuf) {
+        let watch_id = self.inotify.add_watch(&path, IN_MODIFY | IN_DELETE).unwrap();
+        self.watches.insert(watch_id, path);
     }
 
-    pub fn wait_for_events(&mut self) -> &[Event] {
-        self.inotify.wait_for_events().unwrap()
+    pub fn wait_for_events(&mut self) -> Vec<EventPath> {
+        let events = self.inotify.wait_for_events().unwrap();
+        
+        let mut event_paths: Vec<EventPath> = vec![];
+        for event in events.iter() {
+            let path: PathBuf = self.watches.get(&event.wd).unwrap().clone();
+            event_paths.push(EventPath::new(path, event.clone()));
+        }
+
+        event_paths
     }
 
     pub fn close(&mut self) {
         self.inotify.close().unwrap();
     }
 }
+
+pub struct EventPath {
+    event: Event,
+    path: PathBuf
+}
+
+impl EventPath {
+    fn new(path: PathBuf, event: Event) -> EventPath {
+        EventPath {
+            event: event,
+            path: path
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod test {
@@ -50,22 +78,25 @@ mod test {
     #[test]
     fn watch_a_single_file() {
         let (path, mut file) = create_temp_file();
+        let filepath = path.clone();
 
         let mut fw = FilesWatcher::new();
-        let watch = fw.add_file(&path);
+        fw.add_file(path);
 
         write_to(&mut file);
 
         {
-            let events = fw.wait_for_events();
-            assert_eq!(1, events.len());
-            for event in events.iter() {
-                assert_eq!(watch, event.wd);
+            let event_paths = fw.wait_for_events();
+            assert_eq!(1, event_paths.len());
+            for event_path in event_paths.iter() {
+                assert_eq!(filepath, event_path.path);
             }
         }
 
+        for path in fw.watches.values() {
+            remove_temp_file(path);
+        }
         fw.close();
-        remove_temp_file(&path);
     }
 
 
@@ -74,24 +105,28 @@ mod test {
         let (path1, mut file1) = create_temp_file();
         let (path2, mut file2) = create_temp_file();
 
+        let filepath1 = path1.clone();
+        let filepath2 = path2.clone();
+
         let mut fw = FilesWatcher::new();
-        let watch1 = fw.add_file(&path1);
-        let watch2 = fw.add_file(&path2);
+        fw.add_file(path1);
+        fw.add_file(path2);
 
         write_to(&mut file1);
         write_to(&mut file2);
 
         {
-            let events = fw.wait_for_events();
-            assert_eq!(2, events.len());
-            for event in events.iter() {
-                assert!(event.wd == watch1 || event.wd == watch2);
+            let event_paths = fw.wait_for_events();
+            assert_eq!(2, event_paths.len());
+            for event_path in event_paths.iter() {
+                assert!(filepath1 == event_path.path || filepath2 == event_path.path);
             }
         }
 
+        for path in fw.watches.values() {
+            remove_temp_file(path);
+        }
         fw.close();
-        remove_temp_file(&path1);
-        remove_temp_file(&path2);
     }
 
     fn create_temp_file() -> (PathBuf, File) {
@@ -100,7 +135,7 @@ mod test {
             .take(8)
             .collect();
 
-        let filename = "eagle-test-".to_string() + &rand_part;
+        let filename = "eagleeye-test-".to_string() + &rand_part;
         let path = temp_dir().join(filename);
         let file = File::create(&path).unwrap_or_else(|error|
             panic!("Failed to create temporary file: {}", error)
