abf197a8cfa7055545f6c616688e04a8ad428544
Adam Goforth <code@adamgoforth.com>
2016-10-16 08:40:36 +0200

    Add basic config file support
    This commit only supports a single watcher, a single path, and command
    actions. It's also just hacked into main to get something working.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ use actions::print::PrintAction;
 use actions::command::CommandAction;
 use docopt::Docopt;
 use files_watcher::FilesWatcher;
-use toml::Value;
 
 pub mod files_watcher;
 pub mod actions;
@@ -54,6 +53,24 @@ struct Args {
     flag_version: bool,
 }
 
+#[derive(Debug, RustcDecodable)]
+struct Config {
+    settings: Option<SettingsConfig>,
+    watchers: Option<Vec<WatcherSettings>>,
+}
+
+#[derive(Debug, RustcDecodable)]
+struct SettingsConfig {
+    quiet: Option<bool>,
+}
+
+#[derive(Debug, RustcDecodable)]
+struct WatcherSettings {
+    action_type: String,
+    execute: String,
+    path: String,
+}
+
 #[cfg_attr(test, allow(dead_code))]
 fn main() {
     let version_option = Some(VERSION.to_string());
@@ -66,48 +83,66 @@ fn main() {
                          .unwrap_or_else(|e| e.exit());
 
     let mut actions: Vec<Box<Action + 'static>> = vec![];
-
-    if !args.flag_config.is_empty() &&
-        (!args.flag_execute.is_empty() || !args.flag_path.is_empty()) {
-        println!("Error: you can specify a config file or a command to");
-        println!("       execute and a path, but not both.");
-        process::exit(1);
-    }
-
-    if args.flag_execute.is_empty() && !args.flag_path.is_empty() {
-        println!("Error: you have specified a path to watch, but no command");
-        println!("       to execute on changes. Use the -e option to specify");
-        println!("       a command.");
-        process::exit(1);
-    }
-
-    if !args.flag_execute.is_empty() && args.flag_path.is_empty() {
-        println!("Error: you have specified a command to execute, but no");
-        println!("       path to watch. Use the -p option to specify a path.");
-        process::exit(1);
-    }
-
-    if args.flag_config.is_empty() && args.flag_execute.is_empty() {
-        println!("Error: you must specify either a config file or a command");
-        println!("       to execute and a path.");
-        process::exit(1);
-    }
-    
-    if !args.flag_quiet {
-        let print = PrintAction::new();
-        actions.push(Box::new(print));
-    }
-
-    if !args.flag_execute.is_empty() {
-        let command = CommandAction::new(args.flag_execute, args.flag_quiet);
-        actions.push(Box::new(command));
-    }
-
     let mut fw = FilesWatcher::new();
 
-    let path_buf = PathBuf::from(args.flag_path);
-    fw.add_file(path_buf, actions);
+    if !args.flag_config.is_empty() {
+        let mut config_content = String::new();
+        File::open(&args.flag_config).and_then(|mut f| {
+            f.read_to_string(&mut config_content)
+        }).unwrap();
 
+        let config: Config = toml::decode_str(&config_content).unwrap();
+        //println!("{:#?}", config);
+
+        let settings = match config.settings {
+            Some(settings) => settings,
+            None => SettingsConfig { quiet: Some(false) },
+        };
+
+        let quiet_flag = match settings.quiet {
+            Some(quiet_flag) => quiet_flag,
+            None => false,
+        };
+
+        let watchers = match config.watchers {
+            Some(watchers) => watchers,
+            None => {
+                println!("No watchers defined in config file");
+                process::exit(1);
+            }
+        };
+
+        // TODO: convert to for loop to handle multiple watchers
+        if watchers.len() > 0 {
+            let ref watcher = watchers[0];
+
+            match watcher.action_type.as_ref() {
+                "command" => {
+                    let execute_string = watcher.execute.to_owned();
+                    let command = CommandAction::new(execute_string, quiet_flag);
+                    actions.push(Box::new(command));
+                },
+                _ => (),
+            }
+
+            let path_buf = PathBuf::from(watcher.path.to_owned());
+            fw.add_file(path_buf, actions);
+        }
+    } else {
+        if !args.flag_quiet {
+            let print = PrintAction::new();
+            actions.push(Box::new(print));
+        }
+
+        if !args.flag_execute.is_empty() {
+            let command = CommandAction::new(args.flag_execute, args.flag_quiet);
+            actions.push(Box::new(command));
+        }
+
+        let path_buf = PathBuf::from(args.flag_path);
+        fw.add_file(path_buf, actions);
+    }
+    
     loop {
         let result = fw.wait_and_execute();
 
@@ -117,6 +152,6 @@ fn main() {
         }
     }
 
-     //Uncomment this as soon as we have a way of leaving the loop
-     //fw.close();
+    // Uncomment this as soon as we have a way of leaving the loop
+    // fw.close();
 }
